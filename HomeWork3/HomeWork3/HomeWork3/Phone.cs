14ae6a451741382a0e5bf7c4906cb2d2e678b572
JolantaK <jolanta_apele@inbox.lv>
2022-04-26 23:15:20 +0300

    HomeWork Phone

---
+++ HomeWork3/HomeWork3/HomeWork3/Phone.cs
@@ -0,0 +1,24 @@
+﻿
+namespace HomeWork3
+{
+    public class Phone
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Length { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public void Call()
+        {
+        }
+        public void SMS()
+        {
+        }
+    }
+
+}
--- HomeWork3/HomeWork3/HomeWork3/Program.cs
+++ HomeWork3/HomeWork3/HomeWork3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using HomeWork3;
+
+Phone first = new Phone();
+
+Console.WriteLine("Ievadiet telefona marku");
+string brand = Console.ReadLine();
+
+Console.WriteLine("Ievadiet telefona modeli");
+string model = Console.ReadLine();
+
+Console.WriteLine("Ievadiet telefona garumu");
+string lengthText = Console.ReadLine();
+int length = int.Parse(lengthText);
+
+Console.WriteLine("Ievadiet telefona platumu");
+string widthText = Console.ReadLine();
+int width = int.Parse(widthText);
+
+Console.WriteLine("Ievadiet telefona augstumu");
+string heightText = Console.ReadLine();
+int height = int.Parse(heightText);
+
+first.Brand = brand;
+first.Model = model;
+first.Length = length;
+first.Width = width;
+first.Height = height;
+
+Console.WriteLine($"Apsveicam, Jūsu {brand} {model} telefons ar parametriem: garums - {length}; platums - {width}; augstums - {height} ir atrasts!");
