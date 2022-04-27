8d9e28fe81632983a269011bf3c432ee5f0705ba
JolantaK <jolanta_apele@inbox.lv>
2022-04-27 17:07:26 +0300

    not finish

---
+++ HomeWorkProduct3/HomeWorkProduct3/HomeWorkProduct3/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWorkProduct3
+{
+    public class Product
+    
+    {
+        public double Length { get; }
+
+        public double Width { get; }
+
+        public double Height { get; }
+
+        public double Wight { get; set; }
+    }
+}
--- HomeWorkProduct3/HomeWorkProduct3/HomeWorkProduct3/Program.cs
+++ HomeWorkProduct3/HomeWorkProduct3/HomeWorkProduct3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Izveidot klasi prece. Lai izveidotu jebkuru preci, tai obligāti ir jābūt garumam platumam un svaram, kuri nedrīkst tapt meinīti pēc preces izveidošnas. Lietotājs izveido preci. Izvadām visus preces parametrus.
+
+
+using HomeWorkProduct3;
+
+Product first = new Product();
+
+
