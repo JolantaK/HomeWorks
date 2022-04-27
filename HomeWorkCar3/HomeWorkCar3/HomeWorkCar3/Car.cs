5b4a044da6360e7abfe54135baf9298b0726a36c
JolantaK <jolanta_apele@inbox.lv>
2022-04-27 16:33:40 +0300

    Added Car class

---
+++ HomeWorkCar3/HomeWorkCar3/HomeWorkCar3/Car.cs
@@ -0,0 +1,30 @@
+﻿
+namespace HomeWorkCar3
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+
+        public string NumberPlate { get; set; }
+
+        public double Speed { get; set; }
+
+        public void StartToDrive()
+        {
+        }
+
+        public void IncreaseSpeed()
+        {
+        }
+
+        public void StopDriving()
+        {
+        }
+
+        public void Cheer()
+        {
+            Console.WriteLine("Beep, Beep");
+        }
+    }
+
+}
--- HomeWorkCar3/HomeWorkCar3/HomeWorkCar3/Program.cs
+++ HomeWorkCar3/HomeWorkCar3/HomeWorkCar3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Izveidot klasi mašīna. Mašinai ir marka, nummurzīme un ātrums. Mašīna māk sākt braukt, palielināt ātrumu, beigt braukt, samazināt ātrumu. Mašīna māk arī uztaurēt (Izvada konsolē "Beep beep")
+
+using HomeWorkCar3;
+
+Car first = new Car();
+
+Console.WriteLine("Ievadi automašīnas marku");
+string brand = Console.ReadLine();
+
+Console.WriteLine("Ievadi automašīnas numuru");
+string numberPlate = Console.ReadLine();
+
+Console.WriteLine("Ievadi automašīnas ātrumu");
+string speedText = Console.ReadLine();
+double speed = double.Parse(speedText);
+
+first.Brand = brand;
+first.NumberPlate = numberPlate;
+first.Speed = speed;
+
+first.Cheer();
